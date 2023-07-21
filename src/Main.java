d78ac4900abc380f3c7882f900c7aa58def07a3c
baha <iambaha.25@gmail.com>
2023-07-21 20:11:03 +0300

    bird draw from image

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("1.Sayıyı giriniz:");
-        int numberOne = input.nextInt();
-        System.out.println("2.Sayıyı giriniz:");
-        int numberTwo = input.nextInt();
-        System.out.println("3.Sayıyı giriniz:");
-        int numberThree = input.nextInt();
-        int sum = numberOne+numberTwo+numberThree;
-        System.out.println("Girilen sayıların toplamı: " + sum);
+        try(FileReader fr = new FileReader("./resource/bird.txt")) {
+            try(BufferedReader in = new BufferedReader(fr)) {
+                String line;
+                while((line = in.readLine()) != null)
+                {
+                    System.out.println(line);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
